0ab5e5475806fe45a98a3caea7c74a6ae2454d0e
misaacs <michael.isaacs@gmail.com>
2014-12-27 08:05:48 -0800

    1. Added Button to Format Name Text 2. Acitavated Google Maps functionality by uncommenting code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -247,7 +247,6 @@ var education={
 
 $("#education").append(HTMLschoolStart);    // Add a new <div> "education-entry"
 $(".education-entry").append(education["schools"]);
-$("mapDiv").append(googleMap);
 
 console.log(work);
 console.log(education);
